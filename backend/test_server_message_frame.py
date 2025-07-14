2fa534ec3b99743b08eae20e2ad6a6e9f47b9244
chistym17 <mchisty31@gmail.com>
2025-07-15 00:56:04 +0600

    cleaned

--- backend/test_server_message_frame.py
+++
@@ -1,5 +0,0 @@
-try:
-    from pipecat.frames.frames import ServerMessageFrame
-    print("ServerMessageFrame is available.")
-except ImportError:
-    print("ServerMessageFrame is NOT available.") 
